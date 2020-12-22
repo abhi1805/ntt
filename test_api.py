428fbe3506dfd135705e1d77e4af02fc50633045
Abhi <agawabhi@gmail.com>
2020-12-22 05:31:04 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,11 +1,11 @@
-import requests
-import sys
+import requests                     #importing python module for sending requests
+import sys                          #importing python module to manipulate python's runtime environment
 
 try:
-    arg = sys.argv[1]
-    r = requests.get(f'http://localhost:5000/interfaces/{arg}/')
+    arg = sys.argv[1]               #passing an argument input to python
+    r = requests.get(f'http://localhost:5000/interfaces/{arg}/')    #make a get request for the entered interface name
     print(r.text)
 
 except:
-    r = requests.get(f'http://localhost:5000/interfaces')
+    r = requests.get(f'http://localhost:5000/interfaces')           #make a get request for all interfaces
     print(r.text)
